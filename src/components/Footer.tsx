67a0dd61c7f43732c5720644e893cbf2f421433d
sumanDevX19 <contactmrsumansaha@gmail.com>
2025-05-17 14:48:09 +0530

    17.05.25 :  Change in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="border-t border-zinc-300 p-5 text-center">
-        <span>@ 2024 Coder&#39; s Book All rights reserved. </span>
+        <span>@ 2024 Book Marker All rights reserved. </span>
     </footer>
   )
 }
